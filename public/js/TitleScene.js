7bd044c7e92254b064e16de6839a29bb976d85b7
ibrayo1 <ibrayo1128@gmail.com>
2020-01-30 15:59:13 -0600

    added title screen animation

--- public/js/TitleScene.js
+++ public/js/TitleScene.js
@@ -5,16 +5,36 @@ class TitleScene extends Phaser.Scene {
 
     preload() {
         this.load.image('playBtn', 'assets/playbutton.png');
-        this.load.image('titlebackground', 'assets/titlebackground.png');
+        this.load.atlas('title_screen', 'assets/title_screen.png', 'assets/title_screen.json');
         this.load.image('super_maple_title', 'assets/super_maple_title.png');
         this.load.audio('bgm', 'assets/sounds/titleScreenMusic.mp3');
     }
 
     create() {
         // add the background image to the game
-        this.background = this.add.image(0, 0, 'titlebackground')
-        .setOrigin(0, 0)
+        var title_screen = this.physics.add.sprite(0, 0, 'title_screen', 'tile_1.png')
+        .setOrigin(0,0)
         .setDisplaySize(game.config.width, game.config.height);
+
+        // make the title_screen immovable and disable gravity for it
+        title_screen.body.immovable = true;
+        title_screen.body.allowGravity = false;
+
+        // animation for title screen
+        this.anims.create({
+            key: 'title_screen_anim',
+            frameRate: 5,
+            frames: this.anims.generateFrameNames('title_screen',{
+                prefix: 'tile_',
+                suffix: '.png',
+                start: 1,
+                end: 16,
+                zeroPad: 0
+            }),
+            repeat: -1
+        });
+
+        title_screen.anims.play('title_screen_anim'); // play title screen animation
 
         // create and play the title music
         var titleMusic = this.sound.add('bgm');
