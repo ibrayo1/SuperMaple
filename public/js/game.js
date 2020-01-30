90f8ea88e28efe7e0f413c905a9deb7a6e1f6999
ibrayo1 <ibrayo1128@gmail.com>
2020-01-30 14:16:18 -0600

    turned off physics debug mode

--- public/js/game.js
+++ public/js/game.js
@@ -7,7 +7,7 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: {y: 1000},
-            debug: true
+            debug: false
         }
     },
     scene: [TitleScene, PlayGame],
